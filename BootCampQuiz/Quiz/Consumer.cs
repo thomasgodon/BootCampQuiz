fccbc4c7a1db69a96f90774949997d706e275a45
Thomas Godon <thomas.godon@gmail.com>
2018-11-12 18:44:11 +0100

    enum consumer toegevoegd

---
+++ BootCampQuiz/Quiz/Consumer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootCampQuiz.Quiz
+{
+    enum Consumer
+    {
+        /// <summary>
+        /// GFX
+        /// </summary>
+        A = 0,
+
+        /// <summary>
+        /// Score
+        /// </summary>
+        B = 1,
+
+        /// <summary>
+        /// Background
+        /// </summary>
+        C = 2
+    }
+}
