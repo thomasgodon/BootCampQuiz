b8a638b71d23c5d7a4c78dd780c1f151634935fd
Thomas Godon <thomas.godon@gmail.com>
2018-11-12 21:21:20 +0100

    ronde 4 form toegevoegd

---
+++ BootCampQuiz/Forms/frmRonde4.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace BootCampQuiz.Forms
+{
+    partial class frmRonde4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tlpRonde1 = new System.Windows.Forms.TableLayoutPanel();
+            this.nudTeamC = new System.Windows.Forms.NumericUpDown();
+            this.nudTeamB = new System.Windows.Forms.NumericUpDown();
+            this.lblTeamC = new System.Windows.Forms.Label();
+            this.lblTeamB = new System.Windows.Forms.Label();
+            this.nudTeamA = new System.Windows.Forms.NumericUpDown();
+            this.lblTeamA = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.tlpRonde1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTeamC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTeamB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTeamA)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tlpRonde1
+            // 
+            this.tlpRonde1.ColumnCount = 3;
+            this.tlpRonde1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33332F));
+            this.tlpRonde1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tlpRonde1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tlpRonde1.Controls.Add(this.nudTeamC, 2, 1);
+            this.tlpRonde1.Controls.Add(this.nudTeamB, 1, 1);
+            this.tlpRonde1.Controls.Add(this.lblTeamC, 2, 0);
+            this.tlpRonde1.Controls.Add(this.lblTeamB, 1, 0);
+            this.tlpRonde1.Controls.Add(this.nudTeamA, 0, 1);
+            this.tlpRonde1.Controls.Add(this.lblTeamA, 0, 0);
+            this.tlpRonde1.Controls.Add(this.btnReset, 1, 4);
+            this.tlpRonde1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpRonde1.Location = new System.Drawing.Point(10, 10);
+            this.tlpRonde1.Name = "tlpRonde1";
+            this.tlpRonde1.RowCount = 5;
+            this.tlpRonde1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tlpRonde1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tlpRonde1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpRonde1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpRonde1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tlpRonde1.Size = new System.Drawing.Size(780, 430);
+            this.tlpRonde1.TabIndex = 1;
+            // 
+            // nudTeamC
+            // 
+            this.nudTeamC.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.nudTeamC.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudTeamC.Location = new System.Drawing.Point(522, 43);
+            this.nudTeamC.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudTeamC.Name = "nudTeamC";
+            this.nudTeamC.Size = new System.Drawing.Size(255, 31);
+            this.nudTeamC.TabIndex = 5;
+            this.nudTeamC.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // nudTeamB
+            // 
+            this.nudTeamB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.nudTeamB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudTeamB.Location = new System.Drawing.Point(262, 43);
+            this.nudTeamB.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudTeamB.Name = "nudTeamB";
+            this.nudTeamB.Size = new System.Drawing.Size(254, 31);
+            this.nudTeamB.TabIndex = 4;
+            this.nudTeamB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblTeamC
+            // 
+            this.lblTeamC.AutoSize = true;
+            this.lblTeamC.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTeamC.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTeamC.Location = new System.Drawing.Point(522, 0);
+            this.lblTeamC.Name = "lblTeamC";
+            this.lblTeamC.Size = new System.Drawing.Size(255, 40);
+            this.lblTeamC.TabIndex = 3;
+            this.lblTeamC.Text = "TEAM C";
+            this.lblTeamC.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTeamB
+            // 
+            this.lblTeamB.AutoSize = true;
+            this.lblTeamB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTeamB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTeamB.Location = new System.Drawing.Point(262, 0);
+            this.lblTeamB.Name = "lblTeamB";
+            this.lblTeamB.Size = new System.Drawing.Size(254, 40);
+            this.lblTeamB.TabIndex = 2;
+            this.lblTeamB.Text = "TEAM B";
+            this.lblTeamB.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // nudTeamA
+            // 
+            this.nudTeamA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.nudTeamA.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudTeamA.Location = new System.Drawing.Point(3, 43);
+            this.nudTeamA.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudTeamA.Name = "nudTeamA";
+            this.nudTeamA.Size = new System.Drawing.Size(253, 31);
+            this.nudTeamA.TabIndex = 0;
+            this.nudTeamA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblTeamA
+            // 
+            this.lblTeamA.AutoSize = true;
+            this.lblTeamA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTeamA.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTeamA.Location = new System.Drawing.Point(3, 0);
+            this.lblTeamA.Name = "lblTeamA";
+            this.lblTeamA.Size = new System.Drawing.Size(253, 40);
+            this.lblTeamA.TabIndex = 1;
+            this.lblTeamA.Text = "TEAM A";
+            this.lblTeamA.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnReset.Location = new System.Drawing.Point(262, 377);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(254, 50);
+            this.btnReset.TabIndex = 12;
+            this.btnReset.Text = "RESET";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // frmRonde4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tlpRonde1);
+            this.Name = "frmRonde4";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ronde 4";
+            this.Load += new System.EventHandler(this.frmRonde4_Load);
+            this.tlpRonde1.ResumeLayout(false);
+            this.tlpRonde1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTeamC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTeamB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTeamA)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpRonde1;
+        private System.Windows.Forms.NumericUpDown nudTeamC;
+        private System.Windows.Forms.NumericUpDown nudTeamB;
+        private System.Windows.Forms.Label lblTeamC;
+        private System.Windows.Forms.Label lblTeamB;
+        private System.Windows.Forms.NumericUpDown nudTeamA;
+        private System.Windows.Forms.Label lblTeamA;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
---
+++ BootCampQuiz/Forms/frmRonde4.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Svt.Caspar;
+using BootCampQuiz.Quiz;
+
+namespace BootCampQuiz.Forms
+{
+    public partial class frmRonde4 : Form, IQuizForm
+    {
+        // fields
+        private QuizControl _control;
+        private CasparCGDataCollection _dataCollection = new CasparCGDataCollection();
+
+        // _________________________________ CLASS PROPERTIES _________________________________
+
+        /// <summary>
+        /// Referentie van quiz control object dat geinitialiseerd werd in frmBootCamp
+        /// </summary>
+        public QuizControl Control
+        {
+            get
+            {
+                return _control;
+            }
+            set
+            {
+                _control = value;
+                _control.TeamPushed += new EventHandler(Control_TeamPushed);
+            }
+        }
+
+        /// <summary>
+        /// Referentie van caspar device object
+        /// </summary>
+        public CasparDevice Caspar { get; set; }
+
+        // _________________________________ CLASS METHODS _________________________________
+
+        public frmRonde4()
+        {
+            InitializeComponent();
+        }
+
+        private void SetGUI()
+        {
+            // checken of er gedrukt is of gereset
+            if (this.Control.Afgedrukt)
+            {
+                switch (this.Control.AfgedruktTeam.Id)
+                {
+                    case 1:
+                        this.lblTeamA.ForeColor = Color.Red;
+                        break;
+
+                    case 2:
+                        this.lblTeamB.ForeColor = Color.Red;
+                        break;
+
+                    case 3:
+                        this.lblTeamC.ForeColor = Color.Red;
+                        break;
+                }
+            }
+            else
+            {
+                // reset kleuren
+                this.lblTeamA.ForeColor = SystemColors.ControlText;
+                this.lblTeamB.ForeColor = SystemColors.ControlText;
+                this.lblTeamC.ForeColor = SystemColors.ControlText;
+            }
+        }
+
+        // _________________________________ EVENT HANDLERS _________________________________
+
+        private void Control_TeamPushed(object sender, EventArgs e)
+        {
+            this.SetGUI();
+
+            if (this.Control.Afgedrukt)
+            {
+                switch (this.Control.AfgedruktTeam.Id)
+                {
+                    case 1:
+                        this.Caspar.Channels[(int)Consumer.A].Load("Actief_A", false);
+                        break;
+
+                    case 2:
+                        this.Caspar.Channels[(int)Consumer.A].Load("Actief_B", false);
+                        break;
+
+                    case 3:
+                        this.Caspar.Channels[(int)Consumer.A].Load("Actief_C", false);
+                        break;
+                }
+
+                this.Caspar.Channels[(int)Consumer.A].Play();
+            }
+        }
+
+        private void frmRonde4_Load(object sender, EventArgs e)
+        {
+            // team namen invoegen
+            this.lblTeamA.Text = this.Control.TeamA.Naam;
+            this.lblTeamB.Text = this.Control.TeamB.Naam;
+            this.lblTeamC.Text = this.Control.TeamC.Naam;
+
+            // toevoegen databindings
+            this.nudTeamA.DataBindings.Add("Text", this.Control.TeamA, "Punten");
+            this.nudTeamB.DataBindings.Add("Text", this.Control.TeamB, "Punten");
+            this.nudTeamC.DataBindings.Add("Text", this.Control.TeamC, "Punten");
+        }
+    }
+}
